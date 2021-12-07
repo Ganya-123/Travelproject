af73dd5f45da51bee936d2b22086cc7a3bdc6e01
Ganya-123 <ganyatgowda92@gmail.com>
2021-12-07 16:38:31 +0530

    seven commit

--- oslib.js
+++
@@ -1,7 +0,0 @@
-const os=require('os');
-console.log(os.homedir());
-console.log(os.platform());
-console.log(os.networkInterfaces());
-console.log(os.uptime());
-console.log(os.totalmem());
-console.log(os.type());
